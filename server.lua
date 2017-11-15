632e407985fb258769f14bf8e6993f66998274a2
Matthew Bergwall <mbergwall2222@gmail.com>
2017-11-15 15:10:38 +0000

    fixed 911

--- server.lua
+++ server.lua
@@ -1,7 +1,11 @@
 function sendToDiscord(name, message)
   if message == nil or message == '' then return FALSE end
-  PerformHttpRequest('DISCORD_URL', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
+  PerformHttpRequest('https://discordapp.com/api/webhooks/380363801633095681/7Axoyk7JX-i9n_sRIKZ9RPEzllDc6ICTXmMVXhIKh5ryMrD9oIKBQ6bJtgL9qSff8lYf', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
 end
+
+AddEventHandler('chatMessage', function(source, name, message)
+  sendToDiscord(name,message)
+end)
 
 AddEventHandler('playerConnecting', function()
   sendToDiscord('SYSTEM', GetPlayerName(source) .. ' joined.')
