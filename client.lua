d4611a30d50e5e7592c39cc08ccbe1793bebde49
mbergwall2222 <mbergwall2222@gmail.com>
2017-11-15 12:51:33 -0500

    testing2

--- client.lua
+++ client.lua
@@ -5,6 +5,7 @@ AddEventHandler( '911:getLocation', function(name)
   local playerPos = GetEntityCoords( GetPlayerPed( -1 ), true )
   local streetA, streetB = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, playerPos.x, playerPos.y, playerPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
   TriggerServerEvent("911:send911",name,streetA,streetB)
+  ShowNotification("GOT2")
 end)
 
 function ShowNotification( text )
