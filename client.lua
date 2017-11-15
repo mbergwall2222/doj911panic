d168583d1b145e0052d81d62e77ce490b4e6cc10
mbergwall2222 <mbergwall2222@gmail.com>
2017-11-15 12:59:19 -0500

    removed shitty code

--- client.lua
+++ client.lua
@@ -6,6 +6,7 @@ AddEventHandler( '911:getLocation', function(name)
   local streetA, streetB = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, playerPos.x, playerPos.y, playerPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
   TriggerServerEvent("911:send911",name,streetA,streetB)
   ShowNotification("GOT2")
+  Citizen.Trace("test")
 end)
 
 function ShowNotification( text )
--- server.lua
+++ server.lua
@@ -1,6 +1,5 @@
 RegisterServerEvent("911:send911")
 function sendToAPI(name, street,cross)
-  if message == nil or message == '' then return FALSE end
   PerformHttpRequest('https://dojrp-cad.com/api/1.1/wf/911', function(err, text, headers) end, 'POST', json.encode({callerName = name, street = street, cross = cross}), { ['Content-Type'] = 'application/json' , ['Authorization'] = 'Bearer 8309ca222a07ea4a41079c17a946add8'})
 end
 
