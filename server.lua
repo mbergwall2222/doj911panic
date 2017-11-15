3b76b0d8b5978dbf5a7d6ac3601c87296e399f9f
mbergwall2222 <mbergwall2222@gmail.com>
2017-11-15 10:25:46 -0500

    Added 911 API

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
-function sendToDiscord(name, message)
+function sendToAPI(name, message)
   if message == nil or message == '' then return FALSE end
-  PerformHttpRequest('https://discordapp.com/api/webhooks/380363801633095681/7Axoyk7JX-i9n_sRIKZ9RPEzllDc6ICTXmMVXhIKh5ryMrD9oIKBQ6bJtgL9qSff8lYf', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
+  PerformHttpRequest('http://mbergwall.io/cad/911.php', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
 end
 
 AddEventHandler('chatMessage', function(source, name, message)
@@ -13,14 +13,6 @@ AddEventHandler('chatMessage', function(source, name, message)
       CancelEvent()
 
       -- Trigger the client event
-      sendToDiscord('San Andreas Communications',name.." NEEDS EMERGENCY SERVICES (911)")
+      sendToAPI(name,"911")
   end
-end)
-
-AddEventHandler('playerConnecting', function()
-  sendToDiscord('SYSTEM', GetPlayerName(source) .. ' joined.')
-end)
-
-AddEventHandler('playerDropped', function(reason)
-  sendToDiscord('SYSTEM', GetPlayerName(source) .. ' left (' .. reason .. ')')
 end)
