e0cf397f1a8d95ea2f37c2d3093d6db4e0b3b5d5
mbergwall2222 <mbergwall2222@gmail.com>
2017-11-15 13:06:56 -0500

    Testing traces

--- server.lua
+++ server.lua
@@ -1,13 +1,14 @@
 RegisterServerEvent("911:send911")
 function sendToAPI(name, street,cross)
   PerformHttpRequest('https://dojrp-cad.com/api/1.1/wf/911', function(err, text, headers) end, 'POST', json.encode({callerName = name, street = street, cross = cross}), { ['Content-Type'] = 'application/json' , ['Authorization'] = 'Bearer 8309ca222a07ea4a41079c17a946add8'})
+  print("sent")
 end
 
 AddEventHandler('chatMessage', function(source, name, message)
   msg = string.lower( message )
 
   if ( msg == "/911") then
-
+      print("911 received")
       CancelEvent()
 
       TriggerClientEvent('911:getLocation',source,name)
@@ -15,5 +16,6 @@ AddEventHandler('chatMessage', function(source, name, message)
 end)
 
 AddEventHandler('911:send911',function(source,name,street,cross)
+  print("sending 911 call")
   sendToAPI(name,street,cross)
 end)
